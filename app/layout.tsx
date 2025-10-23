7b008ad452c63d5f6809c81aa557598f0d64f920
mrnishant1 <nishantkumaragra@gmail.com>
2025-10-24 01:40:44 +0530

    working on seo-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,16 +12,27 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
 export const metadata: Metadata = {
   title: "LeadZup - Your Digital Solutions Hub",
-  description: "LeadZup provides innovative web solutions, portfolio showcase, and digital services tailored for modern businesses.",
-  keywords: ["LeadZup", "web solutions", "digital services", "tech", "marketing", "marketing solutions", "marketing agency", "personalised ads"],
+  description:
+    "LeadZup provides inovative advertisement solution to the Brands, we work on personalsed advertisement ads modal, we monitor reddit like forums for 24x7, looking for the users that are in need of products that Brands provides we help Brands products to position them into conversation and pitch the products to user.",
+  keywords: [
+    "LeadZup",
+    "web advertisement agency",
+    "digital Marketing",
+    "tech",
+    "marketing",
+    "marketing solutions",
+    "marketing agency",
+    "personalised ads",
+  ],
   authors: [{ name: "Nishant Kumar" }],
   viewport: "width=device-width, initial-scale=1.0",
   themeColor: "#0D9488",
+  other: {
+    "google-site-verification": "ZU59Yq-xG2Jga0ExqdDp44yBs3CtJiUNn8BqoxRp2Xc",
+  },
 };
-
 
 export default function RootLayout({
   children,
